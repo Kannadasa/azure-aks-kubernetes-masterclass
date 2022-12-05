d2c9734f834eca07b7591bf7f167c2a19eed3f6c
Kannadasan Swaminathan <kannadasan_swaminathan@hotmail.com>
2022-12-05 11:30:44 +0000

    empty directory

--- 24-Azure-AKS-Terraform/24-03-Create-AKS-Cluster/terraform-manifests-aks/03-resource-group.tf
+++
@@ -1,9 +0,0 @@
-# Terraform Resource to Create Azure Resource Group with Input Variables defined in variables.tf
-resource "azurerm_resource_group" "aks_rg" {
- location = var.location
-  name     = "${var.resource_group_name}-${var.environment}"
-}
-  
-
-
-
