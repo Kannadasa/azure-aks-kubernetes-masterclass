2599a2fb6ebbc2b8aea35779278d1fc01eb00ccd
Kannadasan Swaminathan <kannadasan_swaminathan@hotmail.com>
2022-12-05 11:34:39 +0000

    pushing to master in Github

---
+++ 24-Azure-AKS-Terraform/24-02-Terraform-Language-Basics/terraform-manifests-aks/03-resource-group.tf
@@ -0,0 +1,8 @@
+# Terraform Resource to Create Azure Resource Group with Input Variables defined in variables.tf
+
+resource "azurerm_resource_group" "aksrg" {
+  location = var.location
+  name     = "${var.resource_group_name}-${var.environment}"
+
+
+}
---
+++ 24-Azure-AKS-Terraform/24-02-Terraform-Language-Basics/terraform-manifests-aks/08-outputs.tf
@@ -0,0 +1,22 @@
+# Create Outputs
+# 1. Resource Group Location
+# 2. Resource Group Id
+# 3. Resource Group Name
+
+
+output "location" {
+  value = azurerm_resource_group.aksrg.location
+
+}
+
+output "resource_group_id" {
+
+  value = azurerm_resource_group.aksrg.id
+
+}
+
+output "resource_group_name" {
+
+  value = azurerm_resource_group.aksrg.name
+
+}
